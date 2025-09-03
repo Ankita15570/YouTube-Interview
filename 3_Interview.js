74ce0b118c5c3eff63d4e7554c0e64afeaf81846
Ankita15570 <anshupatil1901@gmail.com>
2025-09-03 16:23:18 +0530

    3_Interview Done

---
+++ 3_Interview.js
@@ -0,0 +1,17 @@
+// React Developer Interview No-3
+
+// What are the future of react ?
+// One way data binding ?
+// Difference between virtual DOM and real DOM ?
+// Reactjs is framework or library ? //library
+// what is use of exs6 in reactjs ?
+// what are the different  part of component in Reactjs ?
+// What is the difference between functional and class component ? // better?
+// Explain Nodejs ?
+// What is the function useState in reactjs ?
+// What is the Difference between controller and uncontrolled component ?
+// What is the prop drilling in react?
+// Custom hooks ?
+// Strict mode in reactjs ?
+// How to pass data between components ? //beteer redux
+// React Router Works?
