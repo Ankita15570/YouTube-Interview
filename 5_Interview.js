13418c950cf13e895fab130b7ff1dccd745ef79d
Ankita15570 <anshupatil1901@gmail.com>
2025-09-03 16:52:38 +0530

    5_Interview Done

---
+++ 5_Interview.js
@@ -0,0 +1,11 @@
+// ReactJs Developer Interview No-5
+
+// What is doctype in HTML? //use ?
+// Purpose of box sizing in CSS?
+// What is acma script in javascript ?
+// Difference between let , var and const ?
+// Gobal vs local variables ?
+// spread operator in javascript ?
+// What is Arrow function ?
+// Waht is type script ?
+// setTimeout vs setInterval ?
